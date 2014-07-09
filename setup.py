9ec331aa145c9548b4433670e4c1c148bf1eeb76
Job Snijders <job@instituut.net>
2014-07-09 22:03:48 +0200

    Prepare setup.py for pypi cheeseshop

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import os
+import sys
+
+version = "0.4"
+
+if sys.argv[-1] == 'publish':
+    os.system('python2.7 setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
 
 setup(name='rancidtoolkit',
-      version='0.3',
+      version=version,
       description='Functions to parse network devices config files and output \
 specific data',
       author='Marcus Stoegbauer',
       author_email='ms@man-da.de',
-      packages=['rancidtoolkit'])
+      license='MIT',
+      packages=find_packages(),
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python"
+                   ],
+      zip_safe=False)
